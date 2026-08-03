7ad19ca88d95cf1323cc6edd95074fd9c8d83cd0
hoshinomizuki27 <pochi7486@gmail.com>
2026-08-03 20:30:13 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const menu = document.getElementById("nav-menu");
-
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
